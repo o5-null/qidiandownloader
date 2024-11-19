e66e62757b465c56329967a9d4a7050515e50521
o5-null <1517808818@qq.com>
2024-11-19 22:14:43 +0800

    write

--- tool.py
+++ tool.py
@@ -1,23 +1,30 @@
-from yarl import URL
+from w3lib.url import canonicalize_url
 import re
 import difflib
 import Levenshtein as ls#莱文斯坦距离 检查文本相似度
 #url相似度识别算法
-def diff(sample:list,url:str):#根据已经获取的url查找类似url
-    url = re.sub('https*','',url.lower())
-    url = url.replace('.','')
-    url = url.replace('/','')
-    url = url.replace(':','')
+def diff(sample:list,url:str):#根据已经获取的url检查目标url相似度
+    def create_string_serial(need_url:str):#对url序列化
+        need_url = canonicalize_url(need_url)#url规范化
+        need_url.replace('https://','')
+        need_url.replace('http://','')
+        need_url.split('/')
+        return need_url
+
+    def calculate_weighted_sequence(sample_url:str,url:str):
+        similarity_level = []
+        sample_url = create_string_serial(sample_url)#序列化
+        url = create_string_serial(url)
+        bigger_len = lambda x, y: x if x > y else y#返回较大的值
+        for i in range(bigger_len(len(sample_url), len(url))):
+            similar.append(ls.distance(sample_url[i],url[i]))
+
     similar = []
     for sam_url in sample:
-        
-        #去除无效字符
-        sam_url = re.sub('https*','',sam_url.lower())
-        sam_url = sam_url.replace('.','')
-        sam_url = sam_url.replace('/','')
-        sam_url = sam_url.replace(':','')
-        similar.append(ls.jaro_winkler(sam_url,url))
-        #similar.append(difflib.SequenceMatcher(None,sam_url,url).quick_ratio())
+        sam_url = canonicalize_url(sam_url)
+        url = canonicalize_url(url)
+        #similar.append(ls.jaro_winkler(sam_url,url))
+        similar.append(difflib.SequenceMatcher(None,sam_url,url).quick_ratio())
     print(similar)
 
 sam = ['Https://www.biquge.co/2_2292/879982.html',
@@ -25,4 +32,6 @@ sam = ['Https://www.biquge.co/2_2292/879982.html',
        'https://www.biquge.co/2_2292/880016.html',
        'https://www.biquge.co/2_2292/880100.html',
        'https://www.biquge.co/2_2292/880173.html']
+
+
 diff(sam,'https://www.biquge.co/dushixiaoshuo/')
